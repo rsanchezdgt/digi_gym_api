4c2f512f43437a957acb38054f5a31932215a015
Ricardo Sánchez <rsanchezsk@gmail.com>
2025-01-12 21:37:16 -0600

    feat: add new parameters to customer progress

--- src/customer-progress/dto/create-customer-progress.dto.ts
+++ src/customer-progress/dto/create-customer-progress.dto.ts
@@ -8,4 +8,23 @@ export class CreateCustomerProgressDto {
   public porcentajeGrasaCorporal: string;
   public grasaVisceral: string;
   public date: string;
+  // Medidas del test
+  public pechadas: string;
+  public burpees: string;
+  public saltoLargo: string;
+  public flexibilidad: string;
+  public abdominales: string;
+  public carrera: string;
+  public pullUp: string;
+  // Medidas Antropometricas
+  public brazoRelajado: string;
+  public brazoContraido: string;
+  public antebrazo: string;
+  public muneca: string;
+  public torax: string;
+  public cintura: string;
+  public cadera: string;
+  public musloMaximo: string;
+  public musloMedio: string;
+  public pantorrilla: string;
 }
